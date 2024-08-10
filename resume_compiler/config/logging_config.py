81dc897b2b6061752a11cd2e9df570d4ce28a373
Tahir Salkic <tahirsalkic@gmail.com>
2024-08-10 00:29:51 -0400

    feat: Add logging config

---
+++ resume_compiler/config/logging_config.py
@@ -0,0 +1,14 @@
+import logging
+
+def setup_logging():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        filename='log/app.log',
+                        filemode='w')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+
+    logging.getLogger().addHandler(console_handler)
