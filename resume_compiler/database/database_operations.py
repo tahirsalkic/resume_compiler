df83c4fb3b05ae818bd1da9005d45ed55b81ffc4
Tahir Salkic <tahirsalkic@gmail.com>
2024-08-15 02:44:48 +0000

    refactor: Refactor database operations to use job ids instead of urls

--- resume_compiler/database/database_operations.py
+++ resume_compiler/database/database_operations.py
@@ -2,103 +2,113 @@ import logging
 from pymongo import MongoClient
 from pymongo.collection import Collection
 
-from utils.mongodb_utils import get_client, load_mongodb_config
-from utils.helper_functions import clean_bookmarks
+from database.db_helper_functions import get_client, load_mongodb_config
+from utils.helper_functions import get_job_id_from_url
 
 logger = logging.getLogger(__name__)
 
-def get_existing_bookmarks(collection: Collection, bookmarks_list: list[str]) -> set[str]:
-    """Fetch existing bookmarks from the collection"""
+def get_existing_job_ids(collection: Collection, job_ids: list[str]) -> set[str]:
+    """Fetch existing job IDs from the collection"""
     return set(
-        doc['url'] for doc in collection.find({'url': {'$in': bookmarks_list}}, {'url': 1})
+        doc['job_id'] for doc in collection.find({'job_id': {'$in': job_ids}}, {'job_id': 1})
     )
 
-def get_new_bookmarks(client: MongoClient, db_name: str, bookmarks_list: list) -> list:
-    """Get bookmarks new to job_postings collection"""
-    bookmarks_list = clean_bookmarks(bookmarks_list)
-    collection = client[db_name]['job_postings']
+def fetch_new_job_ids(client: MongoClient, db_name: str, bookmark_urls: list) -> list:
+    """Get new job IDs not present in the job_postings collection"""
+    job_ids = [get_job_id_from_url(url) for url in bookmark_urls]
+    job_postings_collection = client[db_name]['job_postings']
 
     try:
-        existing_bookmarks = get_existing_bookmarks(collection, bookmarks_list)
-        new_bookmarks = [bookmark for bookmark in bookmarks_list if bookmark not in existing_bookmarks]
+        existing_job_ids = get_existing_job_ids(job_postings_collection, job_ids)
+        new_job_ids = [id for id in job_ids if id not in existing_job_ids]
 
-        logger.info(f"Found {len(new_bookmarks)} missing bookmarks.") if new_bookmarks else logger.info("All bookmarks are present in the collection.")
-        return new_bookmarks
+        if new_job_ids:
+            logger.info(f"Found {len(new_job_ids)} new job postings.")
+        else:
+            logger.info("All job postings are already present in the collection.")
+        
+        return new_job_ids
     except Exception as e:
-        logger.error(f"An error occurred while checking bookmarks: {e}")
+        logger.error(f"An error occurred while checking job postings: {e}")
         return []
 
-def collect_new_job_postings(bookmarks_list: list) -> None:
-    """Insert new bookmarks into job_postings collection"""
+def collect_new_job_postings(bookmark_urls: list) -> list:
+    """Insert new job postings into the job_postings collection"""
     client = get_client()
     config = load_mongodb_config()
     db_name = config['database']
     
     try:
-        new_bookmarks = get_new_bookmarks(client, db_name, bookmarks_list)
-        if new_bookmarks:
-            bookmark_documents = [{'url': url} for url in new_bookmarks]
-            result = client[db_name]['job_postings'].insert_many(bookmark_documents)
-            logger.info(f"Inserted {len(result.inserted_ids)} bookmarks into the collection.")
+        new_job_ids = fetch_new_job_ids(client, db_name, bookmark_urls)
+        if new_job_ids:
+            new_job_documents = [{'job_id': job_id} for job_id in new_job_ids]
+            insertion_result = client[db_name]['job_postings'].insert_many(new_job_documents)
+            logger.info(f"Inserted {len(insertion_result.inserted_ids)} job postings into the collection.")
         else:
-            logger.info("No new bookmarks to insert.")
+            logger.info("No new job postings to insert.")
     except Exception as e:
-        logger.error(f"An error occurred while inserting bookmarks: {e}")
+        logger.error(f"An error occurred while inserting job postings: {e}")
     finally:
         client.close()
 
-def find_bookmarks_without_skills(client: MongoClient, db_name: str) -> list:
-    """Find bookmarks in job_postings collection without a skills field"""
-    collection = client[db_name]['job_postings']
+    return new_job_ids
+
+def find_documents_missing_field(collection_name: str, field_name: str) -> list:
+    """Find documents in the specified collection that lack a specified field."""
+    client = get_client()
+    config = load_mongodb_config()
+    db_name = config['database']
+    collection = client[db_name][collection_name]
     try:
-        query_result = collection.find({"skills": {"$exists": False}}, {"url": 1})
-        bookmarks_without_skills = [doc['url'] for doc in query_result]
+        query_results = collection.find({field_name: {"$exists": False}}, {"_id": 0, "job_id": 1})
+        documents_missing_field = [doc['job_id'] for doc in query_results]
         
-        logger.info(f"Found {len(bookmarks_without_skills)} bookmarks without skills field.")
-        return bookmarks_without_skills
+        logger.info(f"Found {len(documents_missing_field)} documents without the {field_name} field.")
+        return documents_missing_field
     except Exception as e:
-        logger.error(f"An error occurred while finding bookmarks without skills field: {e}")
+        logger.error(f"An error occurred while finding documents without the {field_name} field: {e}")
         return []
+    finally:
+        client.close()
     
-def propagate_skills_field(client: MongoClient, db_name: str) -> list:
-    """Propagate skills field across documents with matching company and role fields and return URLs of updated documents."""
-    collection = client[db_name]['job_postings']
-    propagated_urls = []
+def propagate_skills_field_across_docs(client: MongoClient, db_name: str) -> list:
+    """Propagate the skills field across documents with matching company and role, and return IDs of updated documents."""
+    job_postings_collection = client[db_name]['job_postings']
+    updated_propagated_job_ids = []
     
     try:
-        pipeline = [
+        aggregation_pipeline = [
             {
                 "$group": {
                     "_id": {"company": "$company", "role": "$role"},
-                    "docs": {"$push": "$$ROOT"},
+                    "documents": {"$push": "$$ROOT"},
                     "skills_count": {"$sum": {"$cond": [{"$ifNull": ["$skills", False]}, 1, 0]}}
                 }
             },
             {"$match": {"skills_count": {"$gte": 1}}}
         ]
         
-        aggregated_docs = list(collection.aggregate(pipeline))
+        aggregated_groups = list(job_postings_collection.aggregate(aggregation_pipeline))
         
-        for group in aggregated_docs:
+        for group in aggregated_groups:
             skills_to_propagate = None
             
-            for doc in group['docs']:
-                if 'skills' in doc:
-                    skills_to_propagate = doc['skills']
+            for document in group['documents']:
+                if 'skills' in document:
+                    skills_to_propagate = document['skills']
                     break
             
             if skills_to_propagate:
-                for doc in group['docs']:
-                    if 'skills' not in doc:
-                        collection.update_one(
-                            {"_id": doc['_id']},
+                for document in group['documents']:
+                    if 'skills' not in document:
+                        job_postings_collection.update_one(
+                            {"_id": document['_id']},
                             {"$set": {"skills": skills_to_propagate}}
                         )
-                        # Assuming each document has a 'url' field
-                        propagated_urls.append(doc['url'])
+                        updated_propagated_job_ids.append(document['job_id'])
         
-        logger.info("Skills field propagated across matching documents.")
+        logger.info("Skills field successfully propagated across matching documents.")
     except Exception as e:
-        logger.error(f"An error occurred while propagating skills field: {e}")
+        logger.error(f"An error occurred while propagating the skills field: {e}")
     
-    return propagated_urls
+    return updated_propagated_job_ids
