4d6adddef4cf5a8a5a170c194ba27b926eac6b5b
Tahir Salkic <tahirsalkic@gmail.com>
2024-08-19 23:48:17 +0000

    feat: Update openai operations to leverage concurrent futures instead of multiprocessing poll

--- resume_compiler/ai/openai_operations.py
+++ resume_compiler/ai/openai_operations.py
@@ -1,12 +1,9 @@
 import logging
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from openai import OpenAI
 from dotenv import load_dotenv
-from multiprocessing import Pool
-from functools import partial
 from ai.ai_helper_functions import load_prompts
-from ai.openai_operations import analyze_description
-from config.settings import load_config
 
 logger = logging.getLogger(__name__)
 
@@ -30,17 +27,23 @@ def create_chat_completion(system_prompt: str, user_prompt: str, model='gpt-4o-m
         response = client.chat.completions.create(model=model, messages=messages, temperature=temperature)
         return response.choices[0].message.content
     except Exception as e:
-        logger.error("Error creating chat completion:", e)
+        logger.error("Error creating chat completion:", exc_info=True)
         raise
 
 def analyze_description(description: str, prompt: str) -> str:
     """Analyze a job description using a given prompt."""
     return create_chat_completion(prompt, description, temperature=0.4)
 
-def skills_analysis(job_descriptions: dict) -> str:
-    """Analyze a job descriptions using a given prompt in parallel."""
-    analyze_with_prompt = partial(analyze_description, prompt="skills_analysis")
-    with Pool() as pool:
-        results = pool.map(analyze_with_prompt, job_descriptions.values())
-    
+def skills_analysis(job_descriptions: dict) -> list:
+    """Analyze job descriptions using a given prompt in parallel."""
+    results = []
+    with ThreadPoolExecutor() as executor:
+        future_to_desc = {executor.submit(analyze_description, desc, "skills_analysis"): desc for desc in job_descriptions.values()}
+        for future in as_completed(future_to_desc):
+            try:
+                result = future.result()
+                results.append(result)
+            except Exception as e:
+                logger.error("Job analysis failed.", exc_info=True)
+
     return results
