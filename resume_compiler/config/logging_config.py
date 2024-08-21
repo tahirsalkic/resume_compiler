8f7335c0526b14b82feb02542297c6083646387d
Tahir Salkic <tahirsalkic@gmail.com>
2024-08-21 15:28:53 +0000

    feat: Update logging to have separate levels for file and console

--- resume_compiler/config/logging_config.py
+++ resume_compiler/config/logging_config.py
@@ -1,14 +1,24 @@
 import logging
+import os
 
-def setup_logging():
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        filename='log/app.log',
-                        filemode='w')
+def setup_logging(log_file='log/app.log', log_level=logging.DEBUG, file_level=logging.INFO, console_level=logging.WARNING) -> logging.Logger:
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(file_level)
+    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
 
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
+    console_handler.setLevel(console_level)
     console_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
     console_handler.setFormatter(console_formatter)
 
-    logging.getLogger().addHandler(console_handler)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    logger.info("Logging is set up.")
+    return logger
